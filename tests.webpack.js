859fc4878fce3c5520a3d80704617d111c3bfbb5
Joel Steres <joel@ymobility.com>
2021-05-27 17:09:27 -0700

    Use test wildcard rather than a bucket import.

--- tests.webpack.js
+++
@@ -1,7 +0,0 @@
-// Browser ES6 Polyfill
-// require('babel/polyfill');
-
-'use strict';
-
-const context = require.context('./test', true, /\.test\.jsx$|\.test\.js$/);
-context.keys().forEach(context);
